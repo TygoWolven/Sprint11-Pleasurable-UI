10fa9d0b36dbdf40451a57971da49f9617896a09
TygoWolven <144010858+TygoWolven@users.noreply.github.com>
2024-05-22 12:28:43 +0200

    Added Service Filtering

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,3 +1,25 @@
+// Functie voor de 'Initiatieven'
+const listVraag = document.getElementById('listVraag'),
+      listAanbod = document.getElementById('listAanbod'),
+	  vraagButton = document.getElementById('vraagButton'),
+	  aanbodButton = document.getElementById('aanbodButton'),
+	  aanbodTitle = document.getElementById('titleAanbod'),
+	  vraagTitle = document.getElementById('titleVraag')
+
+function showList(val) {
+    if(val==1) {
+        listVraag.style.display='grid'
+        listAanbod.style.display='none'
+		vraagTitle.style.display='block'
+		aanbodTitle.style.display='none'
+    } if(val==2) {
+        listVraag.style.display='none'
+        listAanbod.style.display='grid'
+		vraagTitle.style.display='none'
+		aanbodTitle.style.display='block'
+    }
+}
+
 // Selecteer like formulier
 let form = document.querySelector('form.like')
 
@@ -68,49 +90,3 @@ form.addEventListener('submit', function(event) {
 	// automatisch terug naar de standaard POST, wat prima is.
 	event.preventDefault()
 })
-
-
-
-
-
-var vraagButton = document.querySelector('#vraagButton')
-var vraagContent = document.querySelector('.vraagAanbod')
-
-
-vraagButton.addEventListener('click', showVraagContent)
-
-function showVraagContent() {
-	console.log("klikt op de vraag button");
-
-	vraagContent.classList.toggle('active');
-
-	this.classList.toggle('active');
-}
-
-
-
-var aanbodButton = document.querySelector('#aanbodButton')
-var aanbodContent = document.querySelector('.aanbodContent')
-
-aanbodButton.addEventListener('click', showAanbodContent)
-
-function showAanbodContent() {
-	console.log("klikt op de aanbod button");
-	
-	aanbodContent.classList.toggle('active');
-
-	this.classList.toggle('active');
-}
-
-
-
-var likeButton = document.querySelector('.like button')
-var heartIcon = document.querySelector('.heartIcon')
-
-likeButton.addEventListener('click', liked);
-
-function liked() {
-	console.log("liked");
-
-	this.classList.add('liked');
-}
